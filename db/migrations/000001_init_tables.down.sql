3a274093476ecde0a7d881aed3e1fe2a88cebe7a
Letooow <kir.strukov2017@yandex.ru>
2025-11-18 17:26:14 +0300

    fix docker-compose.yml

---
+++ db/migrations/000001_init_tables.down.sql
@@ -0,0 +1,13 @@
+DROP INDEX idx_ut_user_id;
+
+DROP TABLE users_team;
+
+DROP INDEX idx_upr_user_id;
+
+DROP TABLE users_pull_requests;
+
+DROP TABLE pull_requests;
+
+DROP TABLE teams;
+
+DROP TABLE users;
